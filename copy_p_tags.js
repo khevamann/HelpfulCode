b661601c6dabae821aabbf1a458a07e6bac4fa2e
Kheva Mann <khevamann@gmail.com>
2019-10-07 14:20:03 -0700

    Update copy_p_tags.js

--- copy_p_tags.js
+++ copy_p_tags.js
@@ -1,9 +1,24 @@
-var content = "";
-$('iframe').contents().find('.content:eq(1) > p').each(function(){
-content += $(this).text();
+// Copy content from Mindtap Bookmarklet
+window.pages = window.pages || {};
+$('iframe').contents().find('.content').each(function(){
+    var currPage = $(this)[0].attributes[2].nodeValue;
+    if(!window.pages[currPage]){
+      window.pages[currPage] = "";
+      $(this).find('p').each(function(){
+          window.pages[currPage] += $(this).text();
+      });
+      window.pages[currPage] += '\n\n';
+    }
 });
+
+// Merge content
+var content = "";
+for (var key of Object.keys(window.pages)){
+    content += window.pages[key];
+};
 navigator.permissions.query({name: "clipboard-write"}).then(result => {
   if (result.state == "granted" || result.state == "prompt") {
     navigator.clipboard.writeText(content);
-  }
+  };
 });
+alert('Content Copied');
